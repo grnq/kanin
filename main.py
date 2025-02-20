86cb68650253519e90a41511a44f3adf0d05aa05
Hans Granqvist <462922+grnq@users.noreply.github.com>
2025-02-20 14:29:39 -0800

    test commit

---
+++ main.py
@@ -0,0 +1,19 @@
+global some_value
+
+def main(val):
+    some_value = val
+    if (testing):
+       print(f"Testing {some_value}")
+    else:
+       print(f"No testing {some_value}")
+
+def testing():
+    if some_value > 3:
+      return True
+    else:
+      return False
+
+
+if __name__ == "__main__":
+    main(2)
+    main(7)
